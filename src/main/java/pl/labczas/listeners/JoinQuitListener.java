aaf666faf40191bce2b8e161f36b18389b08e98c
wannaberel <labmctiktok@gmail.com>
2026-01-13 14:10:27 +0100

    Update JoinQuitListener.java

--- src/main/java/pl/labczas/listeners/JoinQuitListener.java
+++ src/main/java/pl/labczas/listeners/JoinQuitListener.java
@@ -1,1 +1,26 @@
+package pl.labczas.listeners;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import pl.labczas.LabCzas;
+
+public class JoinQuitListener implements Listener {
+    
+    private final LabCzas plugin;
+    
+    public JoinQuitListener(LabCzas plugin) {
+        this.plugin = plugin;
+    }
+    
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        plugin.getTimeManager().onPlayerJoin(event.getPlayer());
+    }
+    
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        plugin.getTimeManager().onPlayerQuit(event.getPlayer());
+    }
+}
